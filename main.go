b6ab9489ae1476a29a4b54d817292f2ae4b0cfdc
James Lucktaylor <jlucktay+github@gmail.com>
2020-05-30 20:03:19 +0100

    Define instance as per tutorial.
    Following this example:
    https://www.pulumi.com/docs/tutorials/gcp/gce-webserver/
    
    Removed the storage bucket and created the network, firewall, and GCE
    instance.

--- main.go
+++ main.go
@@ -1,20 +1,76 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v3/go/gcp/storage"
+	"errors"
+
+	"github.com/pulumi/pulumi-gcp/sdk/v3/go/gcp/compute"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a GCP resource (Storage Bucket)
-		bucket, err := storage.NewBucket(ctx, "my-bucket", nil)
+		// Create a network
+		network, err := compute.NewNetwork(ctx, "network", &compute.NetworkArgs{})
 		if err != nil {
 			return err
 		}
 
-		// Export the DNS name of the bucket
-		ctx.Export("bucketName", bucket.Url)
+		_, err = compute.NewFirewall(ctx, "firewall", &compute.FirewallArgs{
+			Network: network.ID(),
+			Allows: compute.FirewallAllowArray{
+				compute.FirewallAllowArgs{
+					Protocol: pulumi.String("tcp"),
+					Ports:    pulumi.StringArray{pulumi.String("22")},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create a Virtual Machine Instance
+		computeInstance, err := compute.NewInstance(ctx, "instance", &compute.InstanceArgs{
+			MachineType: pulumi.String("f1-micro"),
+			Zone:        pulumi.String("us-east1-d"),
+			BootDisk: compute.InstanceBootDiskArgs{
+				InitializeParams: compute.InstanceBootDiskInitializeParamsArgs{
+					Image: pulumi.String("debian-cloud/debian-9"),
+				},
+			},
+			NetworkInterfaces: compute.InstanceNetworkInterfaceArray{
+				compute.InstanceNetworkInterfaceArgs{
+					Network: network.ID(),
+
+					// AccessConfigs must include a single empty config to request an ephemeral IP
+					AccessConfigs: compute.InstanceNetworkInterfaceAccessConfigArray{
+						compute.InstanceNetworkInterfaceAccessConfigArgs{},
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Export the name and IP address of the Instance
+		ctx.Export("instanceName", computeInstance.Name)
+		ctx.Export("instanceIP", computeInstance.NetworkInterfaces.Apply(func(input interface{}) (interface{}, error) {
+			xini, ok := input.([]compute.InstanceNetworkInterface)
+			if !ok {
+				return nil, errors.New(("not OK!"))
+			}
+
+			if len(xini) == 0 {
+				return nil, errors.New(("no instance network interfaces!"))
+			}
+
+			if len(xini[0].AccessConfigs) == 0 {
+				return nil, errors.New(("no instance network interface access configs!"))
+			}
+
+			return xini[0].AccessConfigs[0].NatIp, nil
+		}))
+
 		return nil
 	})
 }
