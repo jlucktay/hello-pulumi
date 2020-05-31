5954155d7f342f3a2685d74ced3749fa055cf3b7
James Lucktaylor <jlucktay+github@gmail.com>
2020-05-31 12:53:41 +0100

    Add mocks.

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi/pkg/resource"
+)
+
+type mocks int
+
+func (mocks) NewResource(typeToken, name string, inputs resource.PropertyMap, provider, id string) (string,
+	resource.PropertyMap, error) {
+	return name + "_id", inputs, nil
+}
+
+func (mocks) Call(token string, args resource.PropertyMap, provider string) (resource.PropertyMap, error) {
+	return args, nil
+}
